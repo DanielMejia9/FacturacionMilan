dedafc5b6ad293a735c8b7ccb3adae35ad5a48bc
Daniel Mejia <holadanielmejia@gmail.com>
2018-11-27 04:59:44 -0500

    actualizacion de codigo

---
+++ include/header.php
@@ -0,0 +1,42 @@
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+
+<script type="text/javascript" src="jscript/push.min.js"></script>
+<link rel="stylesheet" href="../css/style.css"/>
+<link rel="shortcut icon" href="favicon.ico"/>
+<link href="css/style_menu.css" rel="stylesheet" type="text/css" />
+<link href="css/dashboard.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/component.css" />
+<script src="jscript/modernizr.custom.js"></script>
+
+<script>
+    
+
+Push.create("Bienvenidos a Admin", {
+    body: "Sistema Administrativo",
+    icon: '/images/icono.png',
+    timeout: 4000,
+    onClick: function () {
+        window.focus();
+        this.close();
+    }
+});
+
+</script>
+
+
+<title>Modulo</title>
+</head>
